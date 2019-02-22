b17048ec762b2be0e0124d0c62f1477e5e641fa7
Siddhant <siddhanta.bhatta92@gmail.com>
2019-02-22 22:58:23 +0530

    Simple flask-restplus application'
    '

---
+++ rest_api/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from flask_restplus import Api, Resource, fields
+
+app = Flask(__name__)
+
+authorizations = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-API-KEY'
+    }
+}
+api = Api(app, authorizations=authorizations)
+
+a_language = api.model('Language', {'language': fields.String('The language.')}) #, 'id': fields.Integer('ID')})
+
+languages = []
+python = {'language': 'Python', 'id': 1}
+languages.append(python)
+
+@api.route('/language')
+class Language(Resource):
+    @api.marshal_with(a_language, envelope='the_data')
+    @api.doc(security='apikey')
+    def get(self):
+        return languages
+    
+    @api.expect(a_language)
+    def post(self):
+        language = api.payload
+        language['id'] = len(languages) + 1
+        languages.append(language)
+        return {'result': 'Language added'}, 201
+ 
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
